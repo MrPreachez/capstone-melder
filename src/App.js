f8a46e677d2433224953efca7ebad7221b4ba1a5
Damon  Chouinard <fengchoui@gmail.com>
2023-02-03 20:39:07 -0800

    rough in of home page, test partials and styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.scss';
+import HomePage from './pages/HomePage/HomePage';
+import CreateProject from './pages/CreateProject/CreateProject';
+import AddInput from './pages/AddInput/AddInput';
+import Results from './pages/Results/Results';
 
 
 
@@ -9,10 +13,10 @@ function App() {
     <div className="App">
       <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={<HomePage/>}/>
+        <Route path="/" element={<HomePage/>}/>
         <Route path="/create" element={<CreateProject />}/>
         <Route path="/input/:inputId" element={<AddInput />}/>
-        <Route path="/result/:resultId" element={<Result />} /> */}
+        <Route path="/result/:resultId" element={<Results />} /> 
       </Routes>
       </BrowserRouter>
 
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,11 +1,38 @@
-import "./HomePage.scss"
+import "./HomePage.scss";
 
-
-function HomePage(){
-
-    return (
-        <></>
-    )
+function HomePage() {
+  return (
+    <main className="section section__homePage">
+      <section className="hero__section">
+        <h1 className="hero__title">MELDER</h1>
+        <h2 className="hero__subheading">
+          Melding the many thoughts, ideas and opinions together
+        </h2>
+      </section>
+      
+        <section className="card__section">
+          <div className="about__card">
+            <h2 className="about__title">MELDER brings minds together.</h2>
+            <p className="about__text">
+              The idea behind MELDER is to use AI to support the process of
+              collective mind melds. Whether your an organization, business, or
+              group, qualitative feedback and opinion can be difficult to make
+              sense of or summarise consisely. The belief behind MELDER is that
+              language modelling may serve as a useful tool for this purpose.
+            </p>
+          </div>
+          <div className="list__card">
+            <h2 className="list__title">Explore Past Melds</h2>
+            <ul className="list__group">
+              <li>What do you think of this...</li>
+              <li>How was the event for you?</li>
+              <li>What do you feel about politics</li>
+            </ul>
+          </div>
+        </section>
+      
+    </main>
+  );
 }
 
 export default HomePage;
