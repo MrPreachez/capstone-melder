5cfe28ff1b4e8da8e474df71c31827e19456138a
Damon  Chouinard <fengchoui@gmail.com>
2023-02-03 23:58:17 -0800

    framework for createproject, and additional resets to headings

--- src/pages/CreateProject/CreateProject.js
+++ src/pages/CreateProject/CreateProject.js
@@ -1,10 +1,33 @@
-import "./CreateProject.scss"
+import "./CreateProject.scss";
 function CreateProject() {
-    return (
-        <div>
-
-        </div>
-    )
+  return (
+    <section>
+      <h1>Build a MELDER</h1>
+      <h2>
+        To launch this project, we've made building your first MELDER extremely
+        simple!
+      </h2>
+      <h3>Step 1: Come up with a project name</h3>
+      <h3>Step 2: Figure out a thought provoking question</h3>
+      <form>
+        <label>PROJECT NAME</label>
+        <input
+          className=""
+          type="text"
+          id="name"
+          name="project"
+          placeholder="Give Your Meld a Name"
+        />
+        <label>THOUGHT PROVOKING QUESTION</label>
+        <textarea
+          className="" 
+          id="msg" 
+          name="question"
+          placeholder="Type your question here">
+        </textarea>
+      </form>
+    </section>
+  );
 }
 
 export default CreateProject;
