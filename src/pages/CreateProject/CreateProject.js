e575a463f4be126b9487c55d875c60bbfde86979
Damon  Chouinard <fengchoui@gmail.com>
2023-02-04 12:11:38 -0800

    createProject structure, content, form, basic styles, color pallette to variables.

--- src/pages/CreateProject/CreateProject.js
+++ src/pages/CreateProject/CreateProject.js
@@ -1,32 +1,62 @@
 import "./CreateProject.scss";
+
 function CreateProject() {
   return (
-    <section>
-      <h1>Build a MELDER</h1>
-      <h2>
-        To launch this project, we've made building your first MELDER extremely
-        simple!
-      </h2>
-      <h3>Step 1: Come up with a project name</h3>
-      <h3>Step 2: Figure out a thought provoking question</h3>
-      <form>
-        <label>PROJECT NAME</label>
-        <input
-          className=""
-          type="text"
-          id="name"
-          name="project"
-          placeholder="Give Your Meld a Name"
-        />
-        <label>THOUGHT PROVOKING QUESTION</label>
-        <textarea
-          className="" 
-          id="msg" 
-          name="question"
-          placeholder="Type your question here">
-        </textarea>
-      </form>
-    </section>
+    <main className="section section__createPage">
+      <section className="create__head">
+        <div className="create__banner">
+          <h1 className="head__title">Let's Create Your Project!</h1>
+        </div>
+
+        <h2 className="head__subhead">Building a MELDER is simple!</h2>
+        <ul className="head__list">
+          <li className="head__steps head__step1">
+            <span>Step 1:</span> Think of a project name
+          </li>
+          <li className="head__steps head__step2">
+            <span>Step 2:</span> Come up with a thought provoking question
+          </li>
+          <li className="head__steps head__step1">
+            <span>Step 3:</span> Fill in the form and press CREATE
+          </li>
+        </ul>
+        <p className="head__text">
+          Lastly, just invite your group to answer your question!
+        </p>
+      </section>
+      <section className="create__form">
+        <form className="addProject">
+          <div className="create__formBorder">
+            <div className="addProject__field addProject__field1">
+              <label className="addProject__label-A">PROJECT NAME</label>
+
+              <input
+                className="addProject__inputA"
+                type="text"
+                id="name"
+                name="project"
+                placeholder="Give Your Meld a Name"
+              />
+            </div>
+
+            <div className="addProject__field addProject__field2">
+              <label className="addProject__label-B">
+                THOUGHT PROVOKING QUESTION
+              </label>
+
+              <textarea
+                className="addProject__inputB"
+                id="msg"
+                name="question"
+                placeholder="Type your question here"
+              ></textarea>
+            </div>
+          </div>
+
+          <button className="create__CTA--button">create</button>
+        </form>
+      </section>
+    </main>
   );
 }
 
