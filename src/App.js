a01c9933251427a86d6910c72dc76c8f0e18e484
Damon  Chouinard <fengchoui@gmail.com>
2023-02-11 23:02:31 -0800

    added header component, currently without functionality on login or sign up.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.scss';
+import Header from './components/Header/Header';
 import HomePage from './pages/HomePage/HomePage';
 import CreateProject from './pages/CreateProject/CreateProject';
 import AddInput from './pages/AddInput/AddInput';
@@ -12,6 +13,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path="/" element={<HomePage/>}/>
         <Route path="/create" element={<CreateProject />}/>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import "./Header.scss";
+
+
+function Header() {
+    return(
+        <section className="header__section">
+            
+            <div className="header__container">
+            <Link className="header__homeLink" to="/">
+            <h3 className="header__logo">MELDER</h3>
+            </Link>
+                
+                <a className="header__login">Login</a>
+                <a className="header__signUp">Sign Up</a>
+
+                
+            </div>
+
+        </section>
+    )
+}
+
+export default Header;
